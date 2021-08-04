ff253933cb4c0675e47943391c4e0f748b91a510
Peter Leite <peterleite.dev@gmail.com>
2021-08-04 10:16:32 -0300

    rota definida

--- src/route.js
+++ src/route.js
@@ -3,3 +3,5 @@ const express = require('express')
 const route = express.Router()
 
 route.get('/', (req, res)=> res.render("index2"))
+
+module.exports = route
--- src/server.js
+++ src/server.js
@@ -1,6 +1,15 @@
 const express = require('express')
+const route = require('./route')
+const path = require('path')
 
 const server = express()
+
+server.set('view engine', 'ejs')
+
+server.set('views', path.join(__dirname, 'views'))
+/// dirbane é o nome da pasta atual
+
+server.use(route)
 
 server.listen(3000, () => console.log('RODANDO'))
 // listen é uma modulo que existe dentro do express
