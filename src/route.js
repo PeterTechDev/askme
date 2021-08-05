98c9ddabdef3003a60f2a628cce2ff6a1f9bdd2f
Peter Leite <peterleite.dev@gmail.com>
2021-08-05 17:41:53 -0300

    faz a transição das paginas index e create room através das rotas

--- src/route.js
+++ src/route.js
@@ -3,9 +3,10 @@ const QuestionController = require('./controllers/QuestionController')
 
 const route = express.Router()
 
-route.get('/', (req, res)=> res.render("index"))
+route.get('/', (req, res)=> res.render("index", {page: 'enter-room'}))
+route.get('/create-room', (req, res)=> res.render("index", {page: 'create-room'}))
+
 route.get('/room', (req, res)=> res.render("room"))
-route.get('/create-room', (req, res)=> res.render("create-room"))
 
 route.post('/room/:room/:question/:action', QuestionController.index)
 
