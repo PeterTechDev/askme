782b2b98126c1874bf9ca64a9b937cd285c19a16
Peter Leite <peterleite.dev@gmail.com>
2021-08-05 09:42:08 -0300

    define rotas para o create room e room

--- src/route.js
+++ src/route.js
@@ -3,5 +3,7 @@ const express = require('express')
 const route = express.Router()
 
 route.get('/', (req, res)=> res.render("index2"))
+route.get('/room', (req, res)=> res.render("room"))
+route.get('/create-room', (req, res)=> res.render("create-room"))
 
 module.exports = route
