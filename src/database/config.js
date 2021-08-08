8b42526d6dc025cd0e8bd078a09a9f35bdb1ec1b
Peter Leite <peterleite.dev@gmail.com>
2021-08-08 00:25:45 -0300

    config inicial do database (sqlite e sqlite3)

---
+++ src/database/config.js
@@ -0,0 +1,9 @@
+const sqlite3 = require("sqlite3");
+const { open } = require("sqlite");
+// desta forma acima o js procura só o open dentro do sqlite e salva em uma variável chamada open
+
+module.exports = ()=> 
+    open({
+        filename: '/src/database/pergunteme.sqlite',
+        driver: sqlite3.Database,
+    });
