e5e86e41fc4495c954a2b9437cb615aad6c8936e
Peter Leite <peterleite.dev@gmail.com>
2021-08-08 00:59:18 -0300

    script para criar numero random da pagina

--- src/controllers/RoomController.js
+++ src/controllers/RoomController.js
@@ -1,6 +1,18 @@
+const Database = require("../database/config")
+
 module.exports = {
-    create(req, res){
-        let roomId = 123456
+    async create(req, res){
+        const db = await Database()
+
+        let roomId = ''
+
+        for(let i = 0; i < 6; i++){
+           roomId += Math.floor(Math.random() * 10).toString()
+        }
+
+        await db.run(``)
+
+        await db.close()
 
         res.redirect(`/room/${roomId}`)
     }
